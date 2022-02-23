d57aee04c58d2f762347a52df86c89c1f98a0fba
tladud123 <yungh888@gmail.com>
2022-02-23 17:26:30 +0900

    Dodge2

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody PlayerRigidbody;
+    public float speed = 8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
